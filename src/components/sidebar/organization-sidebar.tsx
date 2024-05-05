458663263e95d6502b5368baef5c56a6737bfaa2
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-05 20:28:28 +0530

    add: OrganizationSidebar

---
+++ src/components/sidebar/organization-sidebar.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+const OrganizationSidebar: React.FC<Props> = ({}) => {
+  return (
+    <div
+      className="
+        hidden lg:flex flex-col space-y-6
+        w-[206px]
+        pl-5 pt-5"
+    >
+      OrganizationSidebar
+    </div>
+  );
+};
+
+export default OrganizationSidebar;
