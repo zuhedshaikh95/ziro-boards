f239c99ca23a8518347b5f4db277aeb191f7f34c
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-05 16:41:32 +0530

    add: ConvexReactClient

---
+++ src/providers/convex-client-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
