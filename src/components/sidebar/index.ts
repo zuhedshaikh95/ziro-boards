a66ce5ed70178e4704484276d229703dea3116e7
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-11 14:00:46 +0530

    add: export AddOrganization, OrganizationsList, OrganizationItem

--- src/components/sidebar/index.ts
+++ src/components/sidebar/index.ts
@@ -1,2 +1,5 @@
 export { default as Sidebar } from "./sidebar";
 export { default as OrganizationSidebar } from "./organization-sidebar";
+export { default as AddOrganization } from "./add-organization";
+export { default as OrganizationsList } from "./organization-list";
+export { default as OrganizationItem } from "./organization-item";
