4e0fcc99d168f8df321e5f973d98bb006f1a1584
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-17 01:43:27 +0530

    add: Board.Skeleton

--- src/components/global/board-list.tsx
+++ src/components/global/board-list.tsx
@@ -17,7 +17,30 @@ const BoardList: React.FC<Props> = ({ organizationId, query }) => {
   const boards = useQuery(api.boards.get, { organizationId });
 
   if (boards === undefined) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <h2 className="text-3xl">{query.favorites ? "Favorite boards" : "Team boards"}</h2>
+        <div
+          className="
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-4
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            mt-8 pb-10
+            gap-5"
+        >
+          <NewBoardButton organizationId={organizationId} disabled />
+          {Array(10)
+            .fill(null)
+            .map((_, index) => (
+              <BoardCard.Skeleton key={index} />
+            ))}
+        </div>
+      </div>
+    );
   }
 
   if (!boards?.length && query.search) {
