9ff4e7b97f5bcc190b05fba7775d0b354c9f1e00
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-16 20:13:28 +0530

    add: EmptyOrganization, BoardList

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,20 @@
-export default function Dashboard() {
+"use client";
+import { BoardList, EmptyOrganization } from "@/components/global";
+import { useOrganization } from "@clerk/nextjs";
+
+type PageProps = {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+};
+
+export default function Dashboard({ searchParams }: PageProps) {
+  const { organization } = useOrganization();
+
   return (
-    <section className="flex flex-col gap-y-4">
-      <p>Dashboard</p>
+    <section className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrganization /> : <BoardList organizationId={organization.id} query={searchParams} />}
     </section>
   );
 }
