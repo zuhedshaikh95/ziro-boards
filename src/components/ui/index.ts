142fb8024be5ab1b2e5e71fc62888d4edb9dee7a
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-19 18:43:54 +0530

    add: export Button, Input, Skeleton

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,5 @@
 export * as Dialog from "./dialog";
 export * as Tooltip from "./tooltip";
+export { Button } from "./button";
+export { Input } from "./input";
+export { Skeleton } from "./skeleton";
