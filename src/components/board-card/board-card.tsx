34de5faec9801fd03d1039b41618abeac42c7274
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-22 23:44:24 +0530

    fix: Skeleton fallback TypeError

--- src/components/board-card/board-card.tsx
+++ src/components/board-card/board-card.tsx
@@ -81,17 +81,4 @@ const BoardCard = ({
   );
 };
 
-BoardCard.Skeleton = () => {
-  return (
-    <div
-      className="
-        aspect-[100/127]
-        rounded-lg
-        overflow-hidden"
-    >
-      <Skeleton className="h-full w-full" />
-    </div>
-  );
-};
-
 export default BoardCard;
