54211f3014c8f4c44ece73a1628fea3d56152dc6
zuhedshaikh95 <zuhed95@gmail.com>
2024-05-22 21:08:11 +0530

    add: export AlertDialog, DropdownMenu

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,8 @@
 export * as Dialog from "./dialog";
 export * as Tooltip from "./tooltip";
+export * as AlertDialog from "./alert-dialog";
+export * as DropdownMenu from "./dropdown-menu";
+
 export { Button } from "./button";
 export { Input } from "./input";
 export { Skeleton } from "./skeleton";
