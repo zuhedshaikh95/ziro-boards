70f0d1d12e4e522714a927e930ff2f3cd7f35b86
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-08 19:21:34 +0530

    add: update, favorite, unfavorite mutations

--- convex/board.ts
+++ convex/board.ts
@@ -45,8 +45,101 @@ export const remove = mutation({
 
     if (!identity) throw new Error("Unauthorized!");
 
-    // TODO: Later check to delete favorite relation as well
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("favoriteBoards")
+      .withIndex("by_user_board", (q) => q.eq("userId", userId).eq("boardId", args.id))
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
 
     await ctx.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("boards"),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("Unauthorized!");
+
+    const title = args.title.trim();
+
+    if (!title) {
+      throw new Error("`title` arg is required!");
+    }
+
+    if (title.length >= 50) {
+      throw new Error("`title` arg cannot be greater than 50 chars!");
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: title,
+    });
+
+    return board;
+  },
+});
+
+export const favorite = mutation({
+  args: { _id: v.id("boards"), organizationId: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("Unauthorized!");
+
+    const board = await ctx.db.get(args._id);
+
+    if (!board) throw new Error("The board you're looking for may have been renamed, deleted or moved elsewhere!");
+
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("favoriteBoards")
+      .withIndex("by_user_board", (q) => q.eq("userId", userId).eq("boardId", board._id))
+      .unique();
+
+    if (existingFavorite) throw new Error("This board had already been marked as favorite!");
+
+    await ctx.db.insert("favoriteBoards", {
+      userId,
+      boardId: board._id,
+      organizationId: args.organizationId,
+    });
+
+    return board;
+  },
+});
+
+export const unfavorite = mutation({
+  args: { _id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("Unauthorized!");
+
+    const board = await ctx.db.get(args._id);
+
+    if (!board) throw new Error("The board you're looking for may have been renamed, deleted or moved elsewhere!");
+
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("favoriteBoards")
+      .withIndex("by_user_board", (q) => q.eq("userId", userId).eq("boardId", board._id))
+      .unique();
+
+    if (!existingFavorite) throw new Error("This board was never been marked as favorited!");
+
+    await ctx.db.delete(existingFavorite._id);
+
+    return board;
   },
 });
