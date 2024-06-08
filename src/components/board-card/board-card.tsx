19348a92bc9dda2ff82ccac6f367d255c4d9f3dc
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-08 19:25:24 +0530

    fix: Skeleton, toast, api

--- src/components/board-card/board-card.tsx
+++ src/components/board-card/board-card.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Footer, Overlay } from "@/components/board-card";
-import { Skeleton } from "@/components/ui";
+import { Actions } from "@/components/global";
+import { useApiMutation } from "@/hooks";
 import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
+import { MoreHorizontal } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Actions } from "@/components/global";
-import { MoreHorizontal } from "lucide-react";
+import { toast } from "sonner";
+import { api } from "../../../convex/_generated/api";
 
 type Props = {
   _id: string;
@@ -30,10 +32,37 @@ const BoardCard = ({
   title,
 }: Props) => {
   const { userId } = useAuth();
-
   const authorLabel = userId === authorId ? "You" : authorName;
-
   const createdAtLabel = formatDistanceToNow(_creationTime, { addSuffix: true, includeSeconds: true });
+
+  const { mutate: favoriteMutate, pending: pendingFavorite } = useApiMutation(api.board.favorite);
+  const { mutate: unfavoriteMutate, pending: pendingUnfavorite } = useApiMutation(api.board.unfavorite);
+
+  const handleToggleFavorite = async () => {
+    if (isFavorite) {
+      const response = await unfavoriteMutate({ _id });
+
+      if (response.error)
+        toast.error("Something went wrong!", {
+          description: response.error,
+          action: {
+            label: "X",
+            onClick: () => {},
+          },
+        });
+    } else {
+      const response = await favoriteMutate({ _id, organizationId });
+
+      if (response.error)
+        toast.error("Something went wrong!", {
+          description: response.error,
+          action: {
+            label: "X",
+            onClick: () => {},
+          },
+        });
+    }
+  };
 
   return (
     <Link href={`/board/${_id}`} passHref>
@@ -73,8 +102,8 @@ const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={handleToggleFavorite}
+          disabled={pendingFavorite || pendingUnfavorite}
         />
       </div>
     </Link>
