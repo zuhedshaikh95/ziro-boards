167fa70a264395bd88a9e5aa5e77c8c544062537
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-09 21:04:48 +0530

    add: dynamic board page

---
+++ src/app/board/[boardId]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Room from "@/components/global/room";
+import { Canvas, CanvasLoading } from "@/components/board";
+
+type Props = {
+  params: {
+    boardId: string;
+  };
+};
+
+export default async function Board({ params }: Props) {
+  return (
+    <Room roomId={params.boardId} fallback={<CanvasLoading />}>
+      <Canvas boardId={params.boardId} />;
+    </Room>
+  );
+}
