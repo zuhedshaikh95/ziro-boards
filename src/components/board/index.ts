6f5a2331d615f937ce9837b1a5ece057d4ba3e73
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-09 21:12:02 +0530

    add: board components export

---
+++ src/components/board/index.ts
@@ -0,0 +1,5 @@
+export { default as Canvas } from "./canvas";
+export { default as Info } from "./info";
+export { default as Participants } from "./participants";
+export { default as Toolbar } from "./toolbar";
+export { default as CanvasLoading } from "./canvas-loading";
