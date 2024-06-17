6cecd5de6179a6cb771c7d0e29a15218bc49d318
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-17 21:26:48 +0530

    add: real-time active participants

--- src/components/board/participants.tsx
+++ src/components/board/participants.tsx
@@ -1,8 +1,19 @@
+"use client";
 import React from "react";
+
+import { UserAvatar } from "@/components/board";
+import { useOthers, useSelf } from "../../../liveblocks.config";
 
 type Props = {};
 
+const MAX_SHOWN_USERS = 1;
+
 const Participants: React.FC<Props> = ({}) => {
+  const users = useOthers();
+  const user = useSelf();
+
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div
       className="
@@ -14,7 +25,22 @@ const Participants: React.FC<Props> = ({}) => {
         flex items-center
         shadow-md"
     >
-      List of users
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => (
+          <UserAvatar key={connectionId} src={info?.avatar} name={info?.name} fallback={info?.name?.charAt(0) || "M"} />
+        ))}
+
+        {user && (
+          <UserAvatar src={user.info?.avatar} name={`${user.info?.name} (You)`} fallback={user.info?.name?.charAt(0)} />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USERS} more`}
+            fallback={`+ ${users.length - MAX_SHOWN_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
