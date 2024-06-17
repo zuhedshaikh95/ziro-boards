59afcf6a6526e36ad15af871d7b4285ddfa30259
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-17 21:26:11 +0530

    add: export UserAvatar

--- src/components/board/index.ts
+++ src/components/board/index.ts
@@ -3,3 +3,4 @@ export { default as Info } from "./info";
 export { default as Participants } from "./participants";
 export { default as Toolbar } from "./toolbar";
 export { default as CanvasLoading } from "./canvas-loading";
+export { default as UserAvatar } from "./user-avatar";
