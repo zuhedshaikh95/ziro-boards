1a9687f02fe1a50156aad9b7ae124d9af6003dc8
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-17 21:30:01 +0530

    add: export Avatar

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,6 +2,7 @@ export * as Dialog from "./dialog";
 export * as Tooltip from "./tooltip";
 export * as AlertDialog from "./alert-dialog";
 export * as DropdownMenu from "./dropdown-menu";
+export * as Avatar from "./avatar";
 
 export { Button } from "./button";
 export { Input } from "./input";
