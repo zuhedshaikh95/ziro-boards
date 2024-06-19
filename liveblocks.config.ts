c265f7ff95d6a20e9049fe011eafbb78f5e74d07
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-19 12:16:58 +0530

    add: throttle publish, Presence type

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -3,7 +3,7 @@ import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
   authEndpoint: "/api/liveblocks-auth",
-
+  throttle: 16,
   async resolveUsers({ userIds }) {
     return [];
   },
@@ -40,7 +40,7 @@ const client = createClient({
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  // cursor: { x: number, y: number } | null,
+  cursor: { x: number; y: number } | null;
   // ...
 };
 
