cbed77eda66709bd56660e7db7e46c3d4f9bab49
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-19 12:19:09 +0530

    add: export ToolButton, CursorsPresence, Cursor

--- src/components/board/index.ts
+++ src/components/board/index.ts
@@ -4,3 +4,6 @@ export { default as Participants } from "./participants";
 export { default as Toolbar } from "./toolbar";
 export { default as CanvasLoading } from "./canvas-loading";
 export { default as UserAvatar } from "./user-avatar";
+export { default as ToolButton } from "./tool-button";
+export { default as CursorsPresence } from "./cursors-presence";
+export { default as Cursor } from "./cursor";
