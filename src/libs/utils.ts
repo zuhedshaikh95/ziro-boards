1188f6c465746aff26295c6fac3eb514adf4201f
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-19 12:20:52 +0530

    add: pointerEventToCanvasPoint

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,5 +1,8 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
+import React from "react";
 import { twMerge } from "tailwind-merge";
+
+import { CameraT } from "../../types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,3 +13,8 @@ const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 export const connectionIdToColor = (connectionId: number): string => {
   return COLORS[connectionId % COLORS.length];
 };
+
+export const pointerEventToCanvasPoint = (event: React.PointerEvent, camera: CameraT) => ({
+  x: Math.round(event.clientX - camera.x),
+  y: Math.round(event.clientY - camera.y),
+});
