7ee6873d056621e958b64e625aea8d869b7f89ac
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-20 13:29:13 +0530

    add: rgbToHex

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-import { CameraT } from "../../types";
+import { CameraT, ColorT, PointT } from "../../types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -14,7 +14,11 @@ export const connectionIdToColor = (connectionId: number): string => {
   return COLORS[connectionId % COLORS.length];
 };
 
-export const pointerEventToCanvasPoint = (event: React.PointerEvent, camera: CameraT) => ({
+export const pointerEventToCanvasPoint = (event: React.PointerEvent, camera: CameraT): PointT => ({
   x: Math.round(event.clientX - camera.x),
   y: Math.round(event.clientY - camera.y),
 });
+
+export const rgbToHex = (color: ColorT): string => {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
+};
