b65b8ee60779cc2260b223bdfedf87872817c426
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-22 23:56:25 +0530

    add: resizeBounds

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-import { CameraT, ColorT, PointT } from "../../types";
+import { CameraT, ColorT, PointT, SideE, XYWHT } from "../../types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -21,4 +21,35 @@ export const pointerEventToCanvasPoint = (event: React.PointerEvent, camera: Cam
 
 export const rgbToHex = (color: ColorT): string => {
   return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
+};
+
+export const resizeBounds = (bounds: XYWHT, corner: SideE, point: PointT): XYWHT => {
+  const result = {
+    x: bounds.x,
+    y: bounds.y,
+    width: bounds.width,
+    height: bounds.height,
+  } satisfies XYWHT;
+
+  if ((corner & SideE.Left) === SideE.Left) {
+    result.x = Math.min(point.x, bounds.x + bounds.width);
+    result.width = Math.abs(bounds.x + bounds.width - point.x);
+  }
+
+  if ((corner & SideE.Right) === SideE.Right) {
+    result.x = Math.min(point.x, bounds.x);
+    result.width = Math.abs(point.x - bounds.x);
+  }
+
+  if ((corner & SideE.Top) === SideE.Top) {
+    result.y = Math.min(point.y, bounds.y + bounds.height);
+    result.height = Math.abs(bounds.y + bounds.height - point.y);
+  }
+
+  if ((corner & SideE.Bottom) === SideE.Bottom) {
+    result.y = Math.min(point.y, bounds.y);
+    result.height = Math.abs(point.y - bounds.y);
+  }
+
+  return result;
 };
