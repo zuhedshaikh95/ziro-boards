067d6e3174368526293fa36303918404f791b6f4
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-23 21:58:24 +0530

    add: useDeleteLayers, useDisabledScrollBounds

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 export { useApiMutation } from "./useApiMutation";
 export { default as useRenameBoardModal } from "./useRenameBoardModal";
 export { default as useSelectionBounds } from "./useSelectionBounds";
+export { default as useDeleteLayers } from "./useDeleteLayers";
+export { default as useDisabledScrollBounds } from "./useDisabledScrollBounds";
