010ce310b23e2498ba1094baf8d4a704e0f1355c
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-23 21:56:16 +0530

    add: Ellipse, Text, Note, Path

--- src/components/board/layer-preview.tsx
+++ src/components/board/layer-preview.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
 
+import { Ellipse, Note, Path, Rectangle, Text } from "@/components/board";
+import { rgbToHex } from "@/libs/utils";
 import { useStorage } from "../../../liveblocks.config";
 import { LayerE } from "../../../types";
-import { Rectangle } from "@/components/board";
 
 type Props = {
   id: string;
@@ -19,6 +20,27 @@ const LayerPreview: React.FC<Props> = ({ id, onLayerPointerDown, selectionColor 
   switch (layer.type) {
     case LayerE.Rectangle:
       return <Rectangle id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
+    case LayerE.Ellipse:
+      return <Ellipse id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
+    case LayerE.Text:
+      return <Text id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
+    case LayerE.Note:
+      return <Note id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor} />;
+
+    case LayerE.Path:
+      return (
+        <Path
+          points={layer.points}
+          onPointerDown={(event) => onLayerPointerDown(event, id)}
+          x={layer.x}
+          y={layer.y}
+          fill={layer.fill ? rgbToHex(layer.fill) : "#000"}
+          stroke={selectionColor}
+        />
+      );
 
     default:
       console.warn("LayerPreview: Unknown layer type");
