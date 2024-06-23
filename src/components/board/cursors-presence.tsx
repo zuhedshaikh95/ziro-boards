36ba3a65f9e044715a4a31f3a9ebad6eded25d87
zuhedshaikh95 <zuhed95@gmail.com>
2024-06-23 21:54:17 +0530

    add: pencil Drafts

--- src/components/board/cursors-presence.tsx
+++ src/components/board/cursors-presence.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { shallow } from "@liveblocks/client";
 import React from "react";
 
-import { useOthersConnectionIds } from "../../../liveblocks.config";
-import { Cursor } from "@/components/board";
+import { Cursor, Path } from "@/components/board";
+import { rgbToHex } from "@/libs/utils";
+import { useOthersConnectionIds, useOthersMapped } from "../../../liveblocks.config";
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
@@ -16,12 +18,40 @@ const Cursors = () => {
   );
 };
 
-type Props = {};
+const Drafts = () => {
+  const others = useOthersMapped(
+    (others) => ({
+      pencilDraft: others.presence.pencilDraft,
+      penColor: others.presence.penColor,
+    }),
+    shallow
+  );
 
-const CursorsPresence: React.FC<Props> = ({}) => {
   return (
     <>
-      {/* TODO: Draft pencil */}
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? rgbToHex(other.penColor) : "#000"}
+            />
+          );
+        }
+
+        return null;
+      })}
+    </>
+  );
+};
+
+const CursorsPresence = ({}) => {
+  return (
+    <>
+      <Drafts />
       <Cursors />
     </>
   );
